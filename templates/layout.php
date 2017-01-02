a3d28ddc12952e39fe8489fea8ca362c0a035657
Kentaro Kuribayashi <kentarok@gmail.com>
2017-01-02 23:02:23 +0900

    Change feed title

--- templates/layout.php
+++ templates/layout.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="main.css">
-    <link rel="alternate" type="application/rss+xml" title="RSS" href="/feed.xml">
+    <link rel="alternate" type="application/rss+xml" title="antipop.fm - Feed" href="/feed.xml">
     <title><?= $config->site_title() . " - " . $page->metadata()["title"] ?></title>
   </head>
   <body>
